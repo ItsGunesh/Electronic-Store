612a8f536622d5d60de1cc50d320dc2b0e4e44bd
ItsGunesh <itsme.gunesh87@gmail.com>
2025-05-16 10:38:40 +0530

    homeQuantityToggle added

--- homeProductCards.js
+++ homeProductCards.js
@@ -1,16 +1,23 @@
+// import { addToCart } from "./addToCart";
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
 const productContainer = document.querySelector("#productContainer");
 const productTemplate = document.querySelector("#productTemplate");
 
-export const showProductContainer=(products)=>{
-    if(!products) return false;
+export const showProductContainer = (products) => {
+  if (!products) {
+    return false;
+  }
 
-    products.forEach((curProd) => {
-        const { brand, category, description, id, image, name, price, stock } =
+  products.forEach((curProd) => {
+    const { brand, category, description, id, image, name, price, stock } =
       curProd;
 
-      const productClone = document.importNode(productTemplate.content, true);
+    const productClone = document.importNode(productTemplate.content, true);
 
-      productClone.querySelector(".category").textContent = category;
+    productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
+
+    productClone.querySelector(".category").textContent = category;
     productClone.querySelector(".productName").textContent = name;
     productClone.querySelector(".productImage").src = image;
     productClone.querySelector(".productImage").alt = name;
@@ -21,6 +28,18 @@ export const showProductContainer=(products)=>{
       price * 4
     }`;
 
-      productContainer.append(productClone);
-    });
-}
+    productClone
+      .querySelector(".stockElement")
+      .addEventListener("click", (event) => {
+        homeQuantityToggle(event, id, stock);
+      });
+
+    // productClone
+    //   .querySelector(".add-to-cart-button")
+    //   .addEventListener("click", (event) => {
+    //     addToCart(event, id, stock);
+    //   });
+
+    productContainer.append(productClone);
+  });
+};
